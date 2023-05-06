3af96bc779d6b0f8f4d5384fcc8efe91f1c85534
Ry <OfficiallyRy@outlook.com>
2023-05-05 23:58:07 -0600

    Wrote out Drink and Fries schemas

--- NodePractice/server/models/Drink.js
+++ NodePractice/server/models/Drink.js
@@ -3,6 +3,9 @@ import { Schema } from "mongoose";
 
 export const Drink = new Schema(
   {
-    
+    creatorId: {type: Schema.Types.ObjectId, required: true},
+    flavor: {type: String, required: true},
+    size: {type: String, required: true, enum: ['Small', 'Medium', 'Large', 'Super Size']},
+    checkedOut: {type: Boolean, default: false}
   }
 )
