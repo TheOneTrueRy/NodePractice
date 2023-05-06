afb6ec43c979465c7f0f2ea030c3942f43b7ec3c
Ry <OfficiallyRy@outlook.com>
2023-05-05 18:26:21 -0600

    Starting same project as SharpPractice but Node

---
+++ NodePractice/server/models/Burger.js
@@ -0,0 +1,17 @@
+import { Schema } from "mongoose";
+
+
+export const Burger = new Schema(
+  {
+    creatorId: {type: Schema.Types.ObjectId, required: true, ref: 'Account' },
+    buns: {type: Number, required: true},
+    patties: {type: Number, required: true},
+    cheeseSlices: {type: Number, default: 0},
+    pickles: {type: Boolean, default: false},
+    onions: {type: Boolean, default: true},
+    ketchup: {type: Boolean, default: true},
+    specialRequest: {type: String, maxLength: 300},
+    checkedOut: {type: Boolean, default: false}
+  },
+  {timestamps: true, toJSOn: {virtuals: true}}
+)
