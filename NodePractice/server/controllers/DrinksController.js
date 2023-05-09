1989a687c5a76ff69c0b3931233c4b1c315959de
Ryan Wittman <OfficiallyRy@outlook.com>
2023-05-09 18:10:17 -0400

    Can get all drinks or one drink by it's Id

--- NodePractice/server/controllers/DrinksController.js
+++ NodePractice/server/controllers/DrinksController.js
@@ -1,10 +1,33 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
+import { drinksService } from "../services/DrinksService.js";
 
 
 export class DrinksController extends BaseController{
   constructor(){
     super()
     this.router
-    
+    .get('', this.getAllDrinks)
+    .get('/:drinkId', this.getDrinkById)
+    .use(Auth0Provider.getAuthorizedUserInfo)
+  }
+
+  async getAllDrinks(req, res, next){
+    try {
+      const drinks = drinksService.getAllDrinks()
+      res.send(drinks)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getDrinkById(req, res, next){
+    try {
+      const drinkId = req.params.drinkId
+      const drink = drinksService.getDrinkById(drinkId)
+      res.send(drink)
+    } catch (error) {
+      next(error)
+    }
   }
 }
--- NodePractice/server/services/DrinksService.js
+++ NodePractice/server/services/DrinksService.js
@@ -1,7 +1,16 @@
+import { dbContext } from "../db/DbContext.js";
 
 
 class DrinksService{
+  async getAllDrinks(){
+    let drinks = dbContext.Drinks.find()
+    return drinks
+  }
 
+  async getDrinkById(drinkId){
+    let drink = dbContext.Drinks.findById(drinkId)
+    return drink
+  }
 }
 
 export const drinksService = new DrinksService();
