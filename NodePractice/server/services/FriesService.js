2fca9c2c74a2cb7605be8a069f6ab067c590d57e
Ryan Wittman <OfficiallyRy@outlook.com>
2023-05-09 18:28:34 -0400

    Can delete a fries order by it's Id

--- NodePractice/server/services/FriesService.js
+++ NodePractice/server/services/FriesService.js
@@ -1,4 +1,5 @@
 import { dbContext } from "../db/DbContext.js";
+import { Forbidden } from "../utils/Errors.js";
 
 
 class FriesService{
@@ -15,6 +16,15 @@ class FriesService{
   async createFries(friesData){
     let fries = await dbContext.Fries.create(friesData)
     return fries
+  }
+
+  async deleteFriesById(friesId, userId){
+    let fries = await this.getFriesById(friesId)
+    if(fries.creatorId != userId){
+      throw new Forbidden("That's not your fries order to delete!")
+    }
+    await fries.remove()
+    return `Successfully deleted the fries order at id ${fries.id}!`
   }
 }
 
