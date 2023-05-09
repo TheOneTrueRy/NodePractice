64f5a0e53d55ecdc30d2056090d607ee768471f5
Ryan Wittman <OfficiallyRy@outlook.com>
2023-05-08 23:59:19 -0400

    Made it so you can create a burger

--- NodePractice/server/controllers/BurgersController.js
+++ NodePractice/server/controllers/BurgersController.js
@@ -1,10 +1,24 @@
 import BaseController from "../utils/BaseController.js";
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import { burgersService } from "../services/BurgersService.js";
 
 
 export class BurgersController extends BaseController{
   constructor(){
     super()
     this.router
-    
+    .use(Auth0Provider.getAuthorizedUserInfo)
+    .post('', this.createBurger)
+  }
+  async createBurger(req, res, next) {
+    try {
+      const user = req.user
+      const burgerData = req.body
+      burgerData.creatorId = user.id
+      const burger = await burgersService.createBurger(burgerData)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
   }
 }
--- NodePractice/server/services/BurgersService.js
+++ NodePractice/server/services/BurgersService.js
@@ -1,6 +1,11 @@
+import { dbContext } from "../db/DbContext.js";
 
 
 class BurgersService{
+  async createBurger(burgerData) {
+    let burger = await dbContext.Burgers.create(burgerData)
+    return burger
+  }
 
 }
 
