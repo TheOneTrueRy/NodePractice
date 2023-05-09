603a17f93bb68d7bd1e9f9a09ab2ce1dfcff8ad7
Ryan Wittman <OfficiallyRy@outlook.com>
2023-05-09 18:22:39 -0400

    Can now Get All Fries orders

--- NodePractice/server/services/FriesService.js
+++ NodePractice/server/services/FriesService.js
@@ -1,7 +1,11 @@
+import { dbContext } from "../db/DbContext.js";
 
 
 class FriesService{
-
+  async getAllFries(){
+    let fries = await dbContext.Fries.find()
+    return fries
+  }
 }
 
 export const friesService = new FriesService();
