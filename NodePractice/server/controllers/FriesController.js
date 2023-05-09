a6aca81fa126fbca46a4eb708dedc7cb042c1ce0
Ryan Wittman <OfficiallyRy@outlook.com>
2023-05-09 18:17:27 -0400

    stubbed out all routes going to be used for fries

--- NodePractice/server/controllers/FriesController.js
+++ NodePractice/server/controllers/FriesController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@bcwdev/auth0provider"
 import BaseController from "../utils/BaseController.js"
 
 
@@ -5,6 +6,19 @@ export class FiresController extends BaseController{
   constructor(){
     super()
     this.router
-
+    .get('', this.getAllFries)
+    .get('/:friesId', this.getFriesById)
+    .use(Auth0Provider.getAuthorizedUserInfo)
+    .post('', this.createFries)
+    .delete('/:friesId', this.getFriesById)
+  }
+  createFries(arg0, createFries) {
+    throw new Error("Method not implemented.")
+  }
+  getFriesById(arg0, getFriesById) {
+    throw new Error("Method not implemented.")
+  }
+  getAllFries(arg0, getAllFries) {
+    throw new Error("Method not implemented.")
   }
 }
