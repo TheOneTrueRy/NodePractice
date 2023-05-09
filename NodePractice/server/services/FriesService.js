44fde5cb2df507cd4014e9f460ee9134d3e0be87
Ryan Wittman <OfficiallyRy@outlook.com>
2023-05-09 18:24:33 -0400

    Can get a fries order by it's Id

--- NodePractice/server/services/FriesService.js
+++ NodePractice/server/services/FriesService.js
@@ -6,6 +6,11 @@ class FriesService{
     let fries = await dbContext.Fries.find()
     return fries
   }
+
+  async getFriesById(friesId){
+    let fries = await dbContext.Fries.findById(friesId)
+    return fries
+  }
 }
 
 export const friesService = new FriesService();
